fcd5371a22cc0568d9385598542e29bfd7d0d08a
Utente <Utente@Utente-pc.station>
2018-07-01 22:30:00 +0200

    prima soluz

--- Lab08/src/it/polito/tdp/dizionariograph/DizionarioGraphController.java
+++ Lab08/src/it/polito/tdp/dizionariograph/DizionarioGraphController.java
@@ -1,5 +1,149 @@
 package it.polito.tdp.dizionariograph;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.dizionariograph.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class DizionarioGraphController {
+
+	private Model model;
 	
+	
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btngrafo;
+
+    @FXML
+    private Button btnvicini;
+
+    @FXML
+    private Button btngrado;
+
+    @FXML
+    private TextField txtLetters;
+
+    @FXML
+    private TextField txtWord;
+
+    @FXML
+    private TextArea txtLog;
+
+    
+    public void setModel(Model model) {
+		this.model = model;
+		btngrado.setDisable(true); 
+		btnvicini.setDisable(true);
+	
+		
+	}
+
+	@FXML
+    void doGeneraGrafo(ActionEvent event) {
+		//grafo di delle stesse dimensioni della parola cercata 
+		
+		txtLog.clear();
+    	txtWord.clear();
+    	
+    	try {
+    		int numLettere= Integer.parseInt(txtLetters.getText());
+    		model.createGraph(numLettere);
+			System.out.println("numero di Lettere: " + numLettere);
+			
+			btngrafo.setDisable(true); //disattivo il pulsante dopo averlo usato
+			btnvicini.setDisable(false); //li attiva
+			btngrado.setDisable(false);
+		
+       
+			
+		} catch (RuntimeException re) {
+			txtLog.setText("Inserisci numero di lettere");
+		}
+
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+    	txtLog.clear();
+    	txtWord.clear();
+    	txtLetters.clear();
+    	
+    	btngrado.setDisable(true); 
+		btnvicini.setDisable(true);
+		btngrafo.setDisable(false);
+    }
+
+    @FXML
+    void doTrovaGradoMax(ActionEvent event) {
+    	
+    	txtLog.clear();
+    	
+	    try {
+	    	String result=model.findMaxDegree();
+	    	txtLog.appendText(result);
+	    	
+	    	
+	    } catch (RuntimeException re) {
+	    	txtLog.setText("Crea prima grafo");
+		}
+    
+    }
+
+    @FXML
+    void doTrovaVicini(ActionEvent event) {
+    	
+    	txtLog.clear();
+    	
+    	
+    	try {
+    		
+    		String parola= txtWord.getText();
+    		if (parola == null || parola.length() == 0) {
+				txtLog.setText("Inserire una parola da cercare");
+				return;
+			} 
+    		
+    		
+    	txtLog.appendText("Vicini della parola inserita: \n");
+    	List<String> result= new ArrayList<String>();
+    	result= model.displayNeighbours(parola);
+    		
+    	if (result != null) {
+	    	for(String s: result)
+	    	txtLog.appendText(s+"\n");
+		} else {
+			txtLog.setText("Non è stato trovato nessun risultato");
+		}
+    	
+    		
+    		
+    	}catch (RuntimeException re) {
+				txtLog.setText("Crea prima grafo");
+			}
+    		
+
+    }
+
+    @FXML
+    void initialize() {
+        assert txtLetters != null : "fx:id=\"txtLetters\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert txtWord != null : "fx:id=\"txtWord\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert txtLog != null : "fx:id=\"txtLog\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert btngrafo != null : "fx:id=\"btngrafo\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert btnvicini != null : "fx:id=\"btnvicini\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+        assert btngrado != null : "fx:id=\"btngrado\" was not injected: check your FXML file 'DizionarioGraph.fxml'.";
+
+    }
 }
--- Lab08/src/it/polito/tdp/dizionariograph/model/Model.java
+++ Lab08/src/it/polito/tdp/dizionariograph/model/Model.java
@@ -1,23 +1,134 @@
 package it.polito.tdp.dizionariograph.model;
 
 import java.util.ArrayList;
+
 import java.util.List;
+
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleGraph;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import it.polito.tdp.dizionariograph.db.WordDAO;
 
 public class Model {
 
+	private Graph<String, DefaultEdge> graph;
+	private WordDAO dao;
+	private List<String> listaParole;
+	private int numeroLettere = 0;
+	
+	public Model() {
+		dao = new WordDAO();
+	}
+	
+	/*se devo lavorare con singola parola come in questo caso conviene java
+	 *se devo lavorare su elenchi di parole filtrarle e fare operazioni uso query*/
+	
+	
+	//PRIMO PULSANTE CREA GRAFICO
 	public void createGraph(int numeroLettere) {
+		
+//      esempio per il text model al posto di salvare le cose dal dao
+//		listaParole= new ArrayList<String>();
+//		listaParole.add("casa");
+//		listaParole.add("case");
+//		listaParole.add("cara");
+//		listaParole.add("care");
+//		listaParole.add("caro");
+//		listaParole.add("cure");
+//		listaParole.add("fila");
+//		listaParole.add("pila");
+//		listaParole.add("pile");
 
-		System.err.println("createGraph -- TODO");
+		this.numeroLettere = numeroLettere;
+		List<String> paroleSimili= new ArrayList<String>();
+		
+		/*prendo tutte le parole della lunghezza data
+		 * diventeranno i vertiti del mio grafo*/
+		this.listaParole= dao.getAllWordsFixedLength(numeroLettere);
+		
+		//creo grafo e ci aggiungo tutti i vertici
+		this.graph= new SimpleGraph<>(DefaultEdge.class);
+		Graphs.addAllVertices(this.graph, this.listaParole);
+		
+		// Per ogni parola aggiungo un arco di collegamento con le parole
+		// che differiscono per una sola lettera (stessa lunghezza)
+		for(String parola: listaParole) {
+			 paroleSimili = this.getAllSimilarWords(listaParole, parola, numeroLettere);
+			 
+			 //collego i vetici in parole simili alla parola di partenza tramite arch, nel grafo
+			 for(String p: paroleSimili) {
+				 graph.addEdge(parola, p);
+			 }
+		}
+		
+		System.out.println(graph);
+		
 	}
 
+	
+	private List<String> getAllSimilarWords(List<String> listaParole2, String parola, int numeroLettere) {
+		List<String> result= new ArrayList<String>();
+		
+		
+		for(String p: listaParole) {
+			
+			if (p.length() != parola.length())
+				throw new RuntimeException("Le due parole hanno una lunghezza diversa.");
+			
+			int cont=0;
+			
+			for(int i=0; i<parola.length(); i++) {
+				if(parola.charAt(i)==p.charAt(i))
+					cont++;
+			}
+			
+			if(cont==parola.length()-1)
+				result.add(p);
+			}
+		
+		return result;
+	}
+
+
+	//SECONDO PULSANTE PER TROVARE I VICINI
 	public List<String> displayNeighbours(String parolaInserita) {
+		List<String> result= new ArrayList<String>();
+		
+		//controllo lista parole
+		if (numeroLettere != parolaInserita.length())
+			throw new RuntimeException("La parola inserita ha una lunghezza differente rispetto al numero inserito.");
 
-		System.err.println("displayNeighbours -- TODO");
-		return new ArrayList<String>();
+		//verifico se la parola esiste nel mio dizionario
+		if(!listaParole.contains(parolaInserita))
+			throw new RuntimeException("La parola inserita non è presente nella lista di parole.");
+		
+		//vado a cercare tutti i vicini della parola passata nei vertici del grafo
+		for(String p: graph.vertexSet()) { 
+			if(p.equals(parolaInserita))
+				result= Graphs.neighborListOf(graph, p); //funzione che mi restituisce la lista di vicini
+		}
+			
+		return result;
 	}
 
-	public int findMaxDegree() {
-		System.err.println("findMaxDegree -- TODO");
-		return -1;
+	
+	//TERZO PULSANTE PER TROVARE IL GRADO MAX E QUALE SIA IL NODO CON IL GRADO MAX
+	public String findMaxDegree() {
+		int gradoMax=0; //numero di archi
+		String temp = null;
+		
+		for(String p: graph.vertexSet()) {
+			if (graph.degreeOf(p) > gradoMax) {
+			gradoMax= graph.degreeOf(p); //da numero di archi collegati alla parola passata
+			temp=p; //vertice del grafo	con il grado massimo
+			}
+		}
+		
+		return temp+" con grado: "+gradoMax;
+		
 	}
 }
